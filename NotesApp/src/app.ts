f41d90483bb56b39c2ae050d3cf53a00b23027b7
lbat83 <lbz1983@gmail.com>
2018-05-02 15:15:32 -0500

    initial

--- NotesApp/src/app.ts
+++ NotesApp/src/app.ts
@@ -1,5 +1,22 @@
-import { HttpClient } from 'aurelia-fetch-client';
+import { Router, RouterConfiguration } from 'aurelia-router';
 
 export class App {
-  message = 'More to come!';
+  router: Router;
+
+  constructor() { }
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'NotesApp';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: 'index', title: 'Home' },
+      { route: '/about', name: 'about', moduleId: './about/index', title: 'About' },
+      { route: '/users', name: 'users', moduleId: './users/index', title: 'Users' },
+      { route: '/categories', name: 'categories', moduleId: './categories/index', title: 'Categories' },
+      { route: '/notes', name: 'notes', moduleId: './notes/index', title: 'Notes' }
+    ]);
+
+    config.fallbackRoute('home');
+
+    this.router = router;
+  }
 }
